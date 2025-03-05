076b598e4304a34a578d5af06716d66101dc3747
Radosław Malinowski <malinowski.rad@gmail.com>
2025-03-05 17:16:20 +0100

    fix tests after redis change to promises usage

--- tests/unit/RedisServiceTest.php
+++ tests/unit/RedisServiceTest.php
@@ -4,7 +4,7 @@ namespace unit;
 
 use App\Libraries\Redis\RedisService;
 use CodeIgniter\Test\CIUnitTestCase;
-use Config\Services;
+use function React\Async\await;
 
 class RedisServiceTest extends CIUnitTestCase
 {
@@ -29,16 +29,16 @@ class RedisServiceTest extends CIUnitTestCase
     public function testServiceActions(): void
     {
         $this->redisService->save(self::REDIS_KEY, self::REDIS_VALUE);
-        $this->assertEquals(self::REDIS_VALUE, $this->redisService->get(self::REDIS_KEY));
+        $this->assertEquals(self::REDIS_VALUE, await($this->redisService->get(self::REDIS_KEY)));
         $this->redisService->save(self::REDIS_KEY_2, self::REDIS_VALUE_2);
-        $this->assertEquals(self::REDIS_VALUE_2, $this->redisService->get(self::REDIS_KEY_2));
+        $this->assertEquals(self::REDIS_VALUE_2, await($this->redisService->get(self::REDIS_KEY_2)));
 
         $this->redisService->save(self::REDIS_KEY, self::REDIS_VALUE_3);
-        $this->assertEquals(self::REDIS_VALUE_3, $this->redisService->get(self::REDIS_KEY));
-        $this->assertEquals(self::REDIS_VALUE_2, $this->redisService->get(self::REDIS_KEY_2));
+        $this->assertEquals(self::REDIS_VALUE_3, await($this->redisService->get(self::REDIS_KEY)));
+        $this->assertEquals(self::REDIS_VALUE_2, await($this->redisService->get(self::REDIS_KEY_2)));
 
         $this->redisService->delete(self::REDIS_KEY);
-        $this->assertNull($this->redisService->get(self::REDIS_KEY));
-        $this->assertEquals(self::REDIS_VALUE_2, $this->redisService->get(self::REDIS_KEY_2));
+        $this->assertNull(await($this->redisService->get(self::REDIS_KEY)));
+        $this->assertEquals(self::REDIS_VALUE_2, await($this->redisService->get(self::REDIS_KEY_2)));
     }
 }
